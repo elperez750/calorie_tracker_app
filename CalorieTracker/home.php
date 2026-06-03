7ce51fddc4e321a83e7f76f6c649e7d8b471eb5b
David Prentice <PrenticeD@cwu.edu>
2026-06-03 10:46:51 -0700

    edited home page design some more

--- CalorieTracker/home.php
+++ CalorieTracker/home.php
@@ -11,8 +11,18 @@
         <?php include('./view/horizontal_nav_bar.php');?>
         <main>
             <section>
-                <p>Calorie Goal:</p>
-                <div></div>
+                <div class="parent_div">
+                    <p>Calorie Goal:</p>
+                    <div></div>
+                    <p class="first_child_p">34</p>
+                    <p class="second_child_p">100</p>
+                    <ul class = "third_child_ul">
+                        <li><a href="/CalorieTracker/index.php?action=create_calorie_goal">Create Calorie Goal</a></li>
+                    </ul>
+                </div>
+                
+                <p class="food_history">Food History</p>
+                <p>Food name</p>
             </section>
         </main>
         <?php include('./view/footer.php');?>
