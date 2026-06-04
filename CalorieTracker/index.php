6ddb8a7dbea4d05796eda3b633bdb25b6aee3ee1
David Prentice <PrenticeD@cwu.edu>
2026-06-04 11:34:08 -0700

    added search page

--- CalorieTracker/index.php
+++ CalorieTracker/index.php
@@ -20,13 +20,17 @@ if($action == 'home'){
 }
 
 else if($action == 'edit_calorie_goal'){
-    include('./view/calorie_goal_editor.php');
+    include('./pages/calorie_goal_editor.php');
 }
 
 else if($action == 'update_calorie_goal'){
     $calorie_goal = filter_input(INPUT_POST, 'calorie_goal', FILTER_VALIDATE_INT);
     $button_text = "Edit Calorie Goal";
     include('./home.php');
+}
+
+else if($action == 'search'){
+    include('./pages/search.php');
 }
 
 else{
---
+++ CalorieTracker/pages/search.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Calorie Tracker</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="./styles/main.css"/>
+        <link rel="stylesheet" href="./styles/search.css"/>
+    </head>
+
+    <body>
+        <?php include('./view/header.php');?>
+        <?php include('./view/horizontal_nav_bar.php');?>
+        <main>
+            <section>
+                <form action="./index.php?action=search_food" method='POST' name='search_food'>
+                    <label class="minCalLabel" for='minCal'>From:</label>
+                    <input class="minCalInputBox" type='number' id='minCal' name='minCal' min=0 max=50000>
+                    <p>to</p>
+                    <input class="maxCalInputBox" type='number' id='maxCal' name='maxCal' min=0 max=50000>
+                    <label class="maxCalLabel" for='maxCal'>Calories</label>
+                </form>
+                <form action="./index.php?action=search_food" method='POST' name='search_food'>
+                    <label for='search_box'>Search:</label>
+                    <input class="searchInputBox" type='text' id='search_box' name='search_box' required autofocus>
+                    <input class="submitButton" type='submit' id='submit_search' value='submit'>
+                </form>
+            </section>
+        </main>
+        <script src="./scripts/APIcall.js"></script>
+        <?php include('./view/footer.php');?>
+    </body>
+</html>
+
